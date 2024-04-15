b242a4c97fe4e24d56a588056e968b47f8f3c7f4
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-04-15 10:03:56 +0300

    added started config

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ["i.ibb.co", "lh3.googleusercontent.com", "res.cloudinary.com"],
+  },
+};
+
+module.exports = nextConfig;
