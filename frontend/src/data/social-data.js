94e99644c319449b1ca78bedb6cf00043b0bb758
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-02 12:29:52 +0300

    added social-data array

---
+++ frontend/src/data/social-data.js
@@ -0,0 +1,28 @@
+const social_data = [
+    {
+        id: 1,
+        title: 'Facebook',
+        link: 'https://www.facebook.com',
+        icon: 'fa-brands fa-facebook-f'
+    },
+    {
+        id: 2,
+        title: 'Twitter',
+        link: 'https://www.twitter.com',
+        icon: 'fa-brands fa-twitter'
+    },
+    {
+        id: 3,
+        title: 'Linkedin',
+        link: 'https://www.linkedin.com',
+        icon: 'fa-brands fa-linkedin'
+    },
+    {
+        id: 4,
+        title: 'Vimeo',
+        link: 'https://vimeo.com/',
+        icon: 'fa-brands fa-vimeo-v'
+    },
+]
+
+export default social_data
