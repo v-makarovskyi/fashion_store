f7f6a069773a59000fe18498e6fe06077b1beb3f
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-04 18:30:24 +0300

    added toast settings

---
+++ frontend/src/utils/toast.js
@@ -0,0 +1,38 @@
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const notifyError = (message) =>
+  toast.error(message, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+
+  const notifySuccess = (message) =>
+  toast.success(message, {
+    position: "top-center",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+
+  <ToastContainer 
+    position="top-center"
+    autoClose={3000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    pauseOnHover
+    draggable
+  />
+
+  export { notifyError, notifySuccess ,ToastContainer }
