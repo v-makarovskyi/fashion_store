8e3214aac5aa9298a26ea9c097b0eccb2e38e44e
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-04 18:39:52 +0300

    added Loader for PopularProducts

---
+++ frontend/src/components/loaders/home/home-popular-prd-loader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Loader from "../loader";
+
+const SingleLoader = ({ isLoading }) => {
+    return (
+        <div className="col d-flex align-items-center justify-content-center" style={{height: '250px'}}>
+            <Loader loading={isLoading} />
+        </div>
+    )
+}
+
+export const HomePopularProductLoader = ({ isLoading }) => {
+    return (
+        <div className="row row-cols-xl-5 row-cols-lg-5 row-cols-md-4">
+            <SingleLoader isLoading={isLoading} />
+            <SingleLoader isLoading={isLoading} />
+            <SingleLoader isLoading={isLoading} />
+            <SingleLoader isLoading={isLoading} />
+            <SingleLoader isLoading={isLoading} />
+        </div>
+    )
+}
