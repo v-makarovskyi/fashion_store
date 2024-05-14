407318d51ccb28dd06f6fc91c51d0ba42583d5d6
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-14 15:12:04 +0300

    productModal added

--- frontend/src/layout/wrapper.jsx
+++ frontend/src/layout/wrapper.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCartProducts } from "@/redux/features/cartSlice";
 import { ToastContainer } from "react-toastify";
 import BackToTop from "@/components/common/back-to-top";
+import ProductModal from "@/components/common/product-modal";
 
 const Wrapper = ({ children }) => {
+  const dispatch = useDispatch()
+  const { productItem, isModalOpen } = useSelector((state) => state.productModal)
+
+  useEffect(() => {
+    dispatch(getCartProducts())
+  }, [dispatch])
+
   return (
     <div id="wrapper">
       {children}
       <BackToTop />
       <ToastContainer />
+      { productItem && <ProductModal product={productItem} isModalOpen={isModalOpen}/> }
     </div>
   );
 };
