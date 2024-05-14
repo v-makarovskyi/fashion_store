2960043d12f54d1c433030c3a84421bb8207fe64
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-14 15:12:53 +0300

    added Provider Redux

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -1,3 +1,22 @@
-export default function App({Component, pageProps}) {
-    return <Component {...pageProps} />
+import "../styles/index.scss";
+import ReactModal from "react-modal";
+import { Provider } from "react-redux";
+import store from "@/redux/store";
+
+if (typeof window !== "undefined") {
+  require("bootstrap/dist/js/bootstrap");
+}
+
+if (typeof window !== "undefined") {
+  ReactModal.setAppElement("body");
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <div id="root">
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  );
 }
