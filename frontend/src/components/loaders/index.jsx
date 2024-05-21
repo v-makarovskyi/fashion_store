4099666b98e4121e0af95407dc3fb235fc0811f7
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-21 19:01:29 +0300

    added loaders index file

---
+++ frontend/src/components/loaders/index.jsx
@@ -0,0 +1,9 @@
+import HomeCateLoader from "./home/home-cate-loader";
+import HomePopularProductLoader from "./home/home-popular-prd-loader";
+import HomePrdLoader from "./home/home-prd-loader";
+
+export {
+    HomeCateLoader,
+    HomePopularProductLoader,
+    HomePrdLoader
+}
