63f6911de4bbbca73f9ded865a3c9b05c7b39744
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-23 11:22:07 +0300

    create Cart Page

---
+++ frontend/src/pages/cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SEO from '@/components/seo'
+import Header from '@/layout/headers/header'
+import Footer from '@/layout/footers/footer'
+import Wrapper from '@/layout/wrapper'
+import CartArea from '@/components/cart-wishlist/cart-area'
+import CommonBreadcrumb from '@/components/cart-wishlist/breadcrumb/common-breadcrumb'
+
+const Cart = () => {
+  return (
+    <div>cart</div>
+  )
+}
+
+export default Cart
