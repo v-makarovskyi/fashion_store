c6dc4d772056ca312be882f63a3848b991564ac6
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-23 13:05:54 +0300

    added component

--- frontend/src/components/cart-wishlist/breadcrumb/common-breadcrumb.jsx
+++ frontend/src/components/cart-wishlist/breadcrumb/common-breadcrumb.jsx
@@ -1,9 +1,37 @@
+//scss file path: 'frontend/public/assets/scss/components/_breadcrumbs.scss'
+
 import React from "react";
 
-const CommonBreadcrumb = () => {
-    return (
-        <div>CommonBreadcrumb</div>
-    )
-}
+const CommonBreadcrumb = ({
+  title,
+  subtitle,
+  center = false,
+  bg_slr = false,
+}) => {
+  return (
+    <section
+      className={`breadcrumb__area ${
+        center ? "text-center" : ""
+      } include-bg pt-95 pb-50`}
+      style={{ background: bg_slr && "#EFF1F5" }}
+    >
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="breadcrumb__content p-relative z-index-1">
+              <h3 className="breadcrumb__title">{title}</h3>
+              <div className="breadcrumb__list">
+                <span>
+                  <a href="#">Home</a>
+                </span>
+                <span>{subtitle}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default CommonBreadcrumb
+export default CommonBreadcrumb;
