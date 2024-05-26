0874fe09de73154922014994c7e356a863a37820
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-05-26 12:34:45 +0300

    added cart-checkout component

--- frontend/src/components/cart-wishlist/cart-checkout.jsx
+++ frontend/src/components/cart-wishlist/cart-checkout.jsx
@@ -1,9 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import useCartInfo from "@/hooks/use-cart-info";
+import Link from "next/link";
 
 const CartCheckout = () => {
-    return (
-        <div>CartCheckout</div>
-    )
-}
+  const { total } = useCartInfo();
+  const [shipCost, setShipCost] = useState(0);
 
-export default CartCheckout
+  //handle shipping cost
+  function handleShippingCost(value) {
+    if (value === "free") {
+      setShipCost(0);
+    } else {
+      setShipCost(value);
+    }
+  }
+
+  return (
+    <div className="tp-cart-checkout-wrapper">
+      <div className="tp-cart-checkout-top d-flex align-items-center justify-content-between">
+        <span className="tp-cart-checkout-top-title">Subtotal:</span>
+        <span className="tp-cart-checkout-top-price">${total}</span>
+      </div>
+      <div className="tp-cart-checkout-shipping">
+        <h4 className="tp-cart-checkout-shipping-title">Shipping</h4>
+        <div className="tp-cart-checkout-shipping-option-wrapper">
+          <div className="tp-cart-checkout-shipping-option">
+            <input id="flat_rate" type="radio" name="shipping" />
+            <label htmlFor="flat_rate" onClick={() => handleShippingCost(20)}>
+              Flat rate: <span>$20.00</span>
+            </label>
+          </div>
+          <div className="tp-cart-checkout-shipping-option">
+            <input id="local_pickup" type="radio" name="shipping" />
+            <label
+              htmlFor="local_pickup"
+              onClick={() => handleShippingCost(25)}
+            >
+              Local pickup: <span>$25.00</span>
+            </label>
+          </div>
+          <div className="tp-cart-checkout-shipping-option">
+            <input id="free-shipping" type="radio" name="shipping" />
+            <label
+              htmlFor="free-shipping"
+              onClick={() => handleShippingCost("free")}
+            >
+              Free shipping
+            </label>
+          </div>
+        </div>
+      </div>
+      <div className="tp-cart-checkout-total d-flex align-items-center justify-content-between">
+        <span>Total:</span>
+        <span>${(total + shipCost).toFixed(2)}</span>
+      </div>
+      <div className="tp-cart-checkout-proceed">
+        <Link href="/checkout" className="tp-cart-checkout-btn w-100">
+          Proceed to Checkout
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CartCheckout;
