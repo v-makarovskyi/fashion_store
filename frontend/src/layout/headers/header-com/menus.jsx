f3c30311c1c8c12bb773dacafe2dc57d799740fe
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-06-20 14:51:38 +0300

    component rewritten using recursion

--- frontend/src/layout/headers/header-com/menus.jsx
+++ frontend/src/layout/headers/header-com/menus.jsx
@@ -1,133 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import menu_data from "@/data/menu-data";
 import blogData from "@/data/blog-data";
 import Link from "next/link";
 import BlogItem from "@/components/blog/blog-item";
 
-const Menus = () => {
-  const blogs = blogData.filter((b) => b.blog === 'fashion')
+const blogs = blogData.filter((el) => el.blog === "fashion");
+
+function MenuRenderList({ id, menus }) {
+  const targetMenus = menus[id];
 
   return (
-    <ul>
-      {menu_data.map((menu) =>
-        menu.homes ? (
-          <li key={menu.id} className="has-dropdown has-mega-menu">
-            <Link href={menu.link}>{menu.title}</Link>
-            <div className="home-menu tp-mega-menu tp-submenu">
-              <div className="row">
-                {
-                  blogs.map((blog) => (
-                    <div key={blog.id} className="col-xl-4 col-lg-4 col-md-6">
-                      <BlogItem blog={blog} />
-                    </div>
-                  ))
-                }
-              </div>
-            </div>
-          </li>
-        ) : menu.products ? (
-          <li key={menu.id} className="has-dropdown has-mega-menu">
-            <Link href={menu.link}>{menu.title}</Link>
-            <ul className="tp-submenu tp-mega-menu mega-menu-style">
-              {menu.product_pages.map((page, index) => (
-                <li key={index} className="has-dropdown">
-                  <Link href={page.link} className="mega-menu-title">
-                    {page.title}
-                  </Link>
-                  <ul className="tp-submenu">
-                    {page.mega_menus.map((el, index) => (
-                      <li key={index}>
-                        <Link href={el.link}>{el.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ) : menu.sub_menu ? (
-          <li key={menu.id} className="has-dropdown">
-            <Link href={menu.link}>{menu.title}</Link>
-            <ul className="tp-submenu">
-              {
-                menu.sub_menus.map((item, index) => (
-                  <li key={index}>
-                    <Link href={item.link}>{item.title}</Link>
-                  </li>
-                ))
-              }
-            </ul> 
-          </li>
+    <>
+      <li
+        key={targetMenus.id}
+        className={
+          targetMenus.homes || targetMenus.products
+            ? "has-dropdown has-megamenu"
+            : targetMenus.sub_menu
+            ? "has-dropdown"
+            : null
+        }
+      >
+        <Link
+          href={targetMenus?.link}
+          className={
+            targetMenus.id === 3 ||
+            targetMenus.id === 7 ||
+            targetMenus.id === 12 ||
+            targetMenus.id === 18
+              ? "megamenu-title"
+              : null
+          }
+        >
+          {targetMenus?.title}
+        </Link>
+        {targetMenus.childMenus.length > 0 || !targetMenus.homes ? (
+          <ul
+            className={
+              targetMenus.homes || targetMenus.products
+                ? "submenu megamenu megamenu-style"
+                : targetMenus.sub_menu
+                ? "submenu"
+                : null
+            }
+          >
+            {targetMenus.childMenus.map((childId) => (
+              <MenuRenderList key={childId} id={childId} menus={menus} />
+            ))}
+          </ul>
         ) : (
-          <li key={menu.id}>
-            <Link href={menu.link}>{menu.title}</Link>
-          </li>
-        )
-      )}
-    </ul>
+          <div className="submenu megamenu">
+            <div className="row">
+              {blogs.map((b) => (
+                <div className="col-md-4 col-lg-4 col-xl-4">
+                  <BlogItem key={b.id} blog={b} />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </li>
+    </>
   );
-};
+}
 
-export default Menus;
-
-/* const Menus = () => {
-  const blogs = blogData.filter((b) => b.blog === "fashion");
+export default function Menus() {
+  const [menus, setmenus] = useState(menu_data);
+  const root = menus[0].childMenus;
 
   return (
-    <ul>
-      {menu_data.map((menu) =>
-        menu.homes ? (
-          <li key={menu.id} className="has-dropdown has-mega-menu">
-            <Link href={menu.link}>{menu.title}</Link>
-            <div className="home-menu tp-submenu tp-mega-menu">
-              <div className="row">
-                {blogs.map((blog) => (
-                  <div key={blog.id} className="col-xl-4 col-lg-4 col-md-6">
-                    <BlogItem blog={blog} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </li>
-        ) : menu.products ? (
-          <li key={menu.id} className="has-dropdown has-mega-menu">
-            <Link href={menu.link}>{menu.title}</Link>
-            <ul className="tp-submenu tp-mega-menu mega-menu-style-2">
-              {menu.product_pages?.map((page, index) => (
-                <li key={index} className="has-dropdown">
-                  <Link href={page.link} className="mega-menu-title">
-                    {page.title}
-                  </Link>
-                  <ul className="tp-submenu">
-                    {page.mega_menus.map((p, i) => (
-                      <li key={i}>
-                        <Link href={p.link}>{p.title}</Link>
-                      </li>
-                    ))}
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ) : menu.sub_menu ? (
-          <li key={menu.id} className="has-dropdown">
-            <Link href={menu.link}>{menu.title}</Link>
-            <ul className="tp-submenu">
-              {menu.sub_menus.map((p, i) => (
-                <li key={i}>
-                  <Link href={p.link}>{p.title}</Link>
-                </li>
-              ))}
-            </ul>
-          </li>
-        ) : (
-          <li key={menu.id}>
-            <Link href={menu.link}>{menu.title}</Link>
-          </li>
-        )
-      )}
+    <ul className="main-menu__list">
+      {root.map((id) => (
+        <MenuRenderList key={id} id={id} menus={menus} />
+      ))}
     </ul>
   );
-};
-
-export default Menus; */
+}
