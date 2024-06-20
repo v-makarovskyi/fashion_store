67313fba7e095491acb512b21752408d31a986e4
Volodymyr Makarovskyi <makarovskyi.v@gmail.com>
2024-06-20 14:50:23 +0300

    updated classes

--- frontend/src/components/blog/blog-item.jsx
+++ frontend/src/components/blog/blog-item.jsx
@@ -1,39 +1,37 @@
 import React from "react";
-import Image from "next/image";
 import { Tags } from "@/svg";
 import Link from "next/link";
+import Image from "next/image";
 
-const BlogItem = ({ blog }) => {
-  const { id, title, date, img, tags } = blog;
+export default function BlogItemTest({ blog }) {
+  const { id, img, title, date, tags } = blog;
 
   return (
-    <div className="tp-blog-item mb-40">
-      <div className="tp-blog-top p-relative fix">
+    <div className="blog-item mb-40">
+      <div className="blog-item__top fix p-relative">
         <Link href={`/blog-details/${id}`}>
-          <Image src={img} alt={`img-blog-${id}`} />
+          <Image src={img} alt={`blog-img-${id}`} />
         </Link>
-        <div className="tp-blog-item-date">
+        <div className="blog-item__date">
           <span>{date}</span>
         </div>
       </div>
-      <div className="tp-blog-content has-thumbnail">
-        <div className="tp-blog-tags">
+      <div className="blog-item__content has-thumbnail">
+        <div className="blog-item__tags">
           <span>
             <Tags />
           </span>
-          {tags.map((t, i) => (
-            <a key={i} href="#">
+          {tags.map((t, idx) => (
+            <a href="#">
               {t}
-              {i < tags.length - 1 && ","}
+              {idx < tags.length - 1 && ","}
             </a>
           ))}
         </div>
-        <h3 className="tp-blog-title">
-          <Link href={`/blog-detail/${id}`}>{title}</Link>
+        <h3 className="blog-item__title">
+          <Link href={`/blog-details/${id}`}>{title}</Link>
         </h3>
       </div>
     </div>
   );
-};
-
-export default BlogItem;
+}
